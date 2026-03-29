1e7869787ec0e6a3e9b933c9d21056249ed5273c
AwokeKnowing <jamesdavidmorris@gmail.com>
2026-03-28 23:21:42 -0700

    v2

--- src/globalmap.py
+++ src/globalmap.py
@@ -653,7 +653,7 @@ class GlobalMap:
             # Store screen coords for outlines (drawn on final atlas later)
             self._debug_outlines = [
                 ([_ego2scr(c, r) for c, r in
-                  [(10, 75), (162, 75), (162, 161), (10, 161)]],
+                  [(12, 74), (162, 74), (162, 162), (12, 162)]],
                  (0, 165, 255)),
                 ([_ego2scr(c, r) for c, r in
                   [(64, 96), (98, 96), (98, 142), (64, 142)]],
